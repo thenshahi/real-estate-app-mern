8544873e8aa967858a5fc169a52b3149259fc077
Nima Shahi <nima@ahmper.com>
2023-03-01 12:49:41 -0500

    first commit

---
+++ client/src/pages/agent.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { useList } from "@pankod/refine-core";
+import { Box, Typography } from "@pankod/refine-mui";
+
+import { AgentCard } from "components";
+const Agents = () => {
+	const { data, isLoading, isError } = useList({
+		resource: "users",
+	});
+
+	const allAgents = data?.data ?? [];
+
+	if (isLoading) {
+		return <div>Loading...</div>;
+	}
+
+	if (isError) {
+		return <div>Something went wrong!</div>;
+	}
+	console.log(allAgents);
+	return (
+		<Box>
+			<Typography fontSize={25} fontWeight={700} color="#11142d">
+				Agents List
+			</Typography>
+			<Box
+				mt="20px"
+				sx={{
+					display: "flex",
+					flexWrap: "wrap",
+					gap: "20px",
+					backgroundColor: "#fcfcfc",
+				}}
+			>
+				{allAgents.map((agent) => (
+					<AgentCard
+						key={agent._id}
+						id={agent._id}
+						name={agent.name}
+						email={agent.email}
+						avatar={agent.avatar}
+						noOfProperties={agent.allProperties.length}
+					/>
+				))}
+			</Box>
+		</Box>
+	);
+};
+
+export default Agents;
---
+++ server/routes/user.routes.js
@@ -0,0 +1,16 @@
+import express from "express";
+
+//IMPORT ALL THE CONTROLLERS ...
+import {
+	createUser,
+	getAllUsers,
+	getUserInfoById,
+} from "../controllers/user.controller.js";
+
+const router = express.Router();
+
+router.route("/").get(getAllUsers);
+router.route("/").post(createUser);
+router.route("/:id").get(getUserInfoById);
+
+export default router;
